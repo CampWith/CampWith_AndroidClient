dd7156d3ae8bb45465be9e334e0d99b967fc0ee0
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-04-07 00:29:28 +0900

    add - 테스트용 지역선택 다이얼로그 추가

---
+++ app/src/main/java/com/example/campwith/presentation/main/CityDialogFragment.kt
@@ -0,0 +1,13 @@
+package com.example.campwith.presentation.main
+
+import android.os.Bundle
+import com.example.campwith.R
+import com.example.campwith.databinding.FragmentCityDialogBinding
+import com.example.campwith.presentation.base.BaseDialogFragment
+
+class CityDialogFragment : BaseDialogFragment<FragmentCityDialogBinding>(R.layout.fragment_city_dialog) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/campwith/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/main/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import com.example.campwith.R
 import com.example.campwith.databinding.ActivityMainBinding
 import com.example.campwith.presentation.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        button.setOnClickListener {
+            val cityDialogFragment = CityDialogFragment()
+            cityDialogFragment.show(supportFragmentManager, "dialog")
+        }
 
     }
 }
