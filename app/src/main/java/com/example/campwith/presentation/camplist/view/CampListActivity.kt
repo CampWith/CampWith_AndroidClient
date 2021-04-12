1bb36bf0fbda9b673d03e15f862eb3b04c455e6b
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-04-12 12:08:29 +0900

    add - 네이버 지도 api 사용 설정

--- app/src/main/java/com/example/campwith/presentation/camplist/view/CampListActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/camplist/view/CampListActivity.kt
@@ -8,6 +8,7 @@ import com.example.campwith.databinding.ActivityCampListBinding
 import com.example.campwith.presentation.base.BaseActivity
 import com.example.campwith.presentation.camplist.viewmodel.CampListViewModel
 import com.example.campwith.presentation.camplist.adapter.CampListAdapter
+import com.naver.maps.map.NaverMapSdk
 import kotlinx.android.synthetic.main.activity_camp_list.*
 
 class CampListActivity : BaseActivity<ActivityCampListBinding>(R.layout.activity_camp_list) {
@@ -17,6 +18,8 @@ class CampListActivity : BaseActivity<ActivityCampListBinding>(R.layout.activity
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("클라이언트 아이디")
 
         campListViewModel = ViewModelProvider(this).get(CampListViewModel::class.java)
         campListViewModel.getCampList()
