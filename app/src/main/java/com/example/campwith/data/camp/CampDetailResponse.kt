b11bfe7ee2833a2e79803c6e96f3683ec0fd0428
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-10 20:07:28 +0900

    add - 캠핑장 세부 데이터 Response 추가

---
+++ app/src/main/java/com/example/campwith/data/camp/CampDetailResponse.kt
@@ -0,0 +1,17 @@
+package com.example.campwith.data.camp
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class CampDetailResponse(
+    val _id: String,
+    val addr1: String,
+    val doNm: String,
+    val facltNm: String,
+    val firstImageUrl: String,
+    val mapX: Double,
+    val mapY: Double,
+    val sigunguNm: String,
+    val tel: String
+) : Parcelable
