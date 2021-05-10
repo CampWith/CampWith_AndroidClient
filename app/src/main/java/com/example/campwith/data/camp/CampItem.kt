c797628021bab25335fa391179e7679f4cc02f0b
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-10 20:06:00 +0900

    fix - 불필요한 코드 삭제

--- app/src/main/java/com/example/campwith/data/camp/CampItem.kt
+++ app/src/main/java/com/example/campwith/data/camp/CampItem.kt
@@ -5,10 +5,9 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class CampItem(
+    val _id: String,
     val addr1: String,
     val doNm: String,
     val facltNm: String,
-    val mapX: Double,
-    val mapY: Double,
     val firstImageUrl: String
 ): Parcelable
