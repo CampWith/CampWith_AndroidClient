32739653d116a5b561e71e6053a2c1e960afc51b
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-11 12:28:20 +0900

    fix - 불필요한 코드 삭제

--- app/src/main/java/com/example/campwith/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/main/view/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.campwith.presentation.main.view
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.campwith.R
-import com.example.campwith.databinding.ActivityMainBinding
-import com.example.campwith.presentation.base.BaseActivity
-import com.example.campwith.presentation.main.view.CityDialogFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         ll_container.setOnClickListener {
             val cityDialogFragment = CityDialogFragment()
--- app/src/main/java/com/example/campwith/presentation/main/viewmodel/CityDialogFragmentViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.campwith.presentation.main.viewmodel
-
-import com.example.campwith.presentation.base.BaseViewModel
-
-class CityDialogFragmentViewModel : BaseViewModel() {
-}
