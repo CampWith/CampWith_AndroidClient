952e4cf5ef5a61291050a19ebf28b5ddd66fc5b6
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-12 23:10:22 +0900

    fix - 불필요한 코드 삭제

--- app/src/main/java/com/example/campwith/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/base/BaseActivity.kt
@@ -5,7 +5,6 @@ import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
-import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseActivity<DB : ViewDataBinding, VM : BaseViewModel>
     (@LayoutRes private val layoutResId: Int) : AppCompatActivity() {
--- app/src/main/java/com/example/campwith/presentation/base/BaseFragment.kt
+++ app/src/main/java/com/example/campwith/presentation/base/BaseFragment.kt
@@ -1,7 +1,6 @@
 package com.example.campwith.presentation.base
 
 import android.os.Bundle
-import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,6 @@ import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
-import io.reactivex.disposables.CompositeDisposable
 
 abstract class BaseFragment<B: ViewDataBinding, VM: BaseViewModel>
     (@LayoutRes private val layoutResId: Int) : Fragment()
