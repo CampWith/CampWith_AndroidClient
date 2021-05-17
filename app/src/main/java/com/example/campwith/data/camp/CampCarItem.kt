6798a3cdaea9ed077b5649877181cb220f73054d
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-17 14:06:13 +0900

    add - 캠핑카 리스폰스 추가

---
+++ app/src/main/java/com/example/campwith/data/camp/CampCarItem.kt
@@ -0,0 +1,15 @@
+package com.example.campwith.data.camp
+
+import android.annotation.SuppressLint
+import kotlinx.android.parcel.Parcelize
+import android.os.Parcelable
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class CampCarItem(
+    val __v: Int,
+    val _id: String,
+    val price: String,
+    val title: String,
+    val url: String
+) : Parcelable
---
+++ app/src/main/java/com/example/campwith/data/camp/CampCarResponse.kt
@@ -0,0 +1,3 @@
+package com.example.campwith.data.camp
+
+class CampCarResponse : ArrayList<CampCarItem>()
