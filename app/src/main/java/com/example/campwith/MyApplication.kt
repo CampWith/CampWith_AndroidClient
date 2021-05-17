fd2ab498ae650679f38ecfee9c6c3b92e1fbbac3
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-17 14:09:34 +0900

    refactor - MyApplication에서 클라이언트 아이디 지정하도록 수정

--- app/src/main/java/com/example/campwith/MyApplication.kt
+++ app/src/main/java/com/example/campwith/MyApplication.kt
@@ -2,6 +2,7 @@ package com.example.campwith
 
 import android.app.Application
 import com.example.campwith.di.myDiModule
+import com.naver.maps.map.NaverMapSdk
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
@@ -9,6 +10,9 @@ import org.koin.core.context.startKoin
 class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("acdbyskku1")
+
         startKoin {
             androidLogger()
             androidContext(this@MyApplication)
--- app/src/main/java/com/example/campwith/presentation/camplist/view/CampListActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/camplist/view/CampListActivity.kt
@@ -5,9 +5,8 @@ import androidx.lifecycle.Observer
 import com.example.campwith.R
 import com.example.campwith.databinding.ActivityCampListBinding
 import com.example.campwith.presentation.base.BaseActivity
-import com.example.campwith.presentation.camplist.viewmodel.CampListViewModel
 import com.example.campwith.presentation.camplist.adapter.CampListAdapter
-import com.naver.maps.map.NaverMapSdk
+import com.example.campwith.presentation.camplist.viewmodel.CampListViewModel
 import kotlinx.android.synthetic.main.activity_camp_list.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -19,8 +18,6 @@ class CampListActivity :
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        NaverMapSdk.getInstance(this).client =
-            NaverMapSdk.NaverCloudPlatformClient("acdbyskku1")
 
         val region = intent.getStringExtra("doName")
         tv_do_name.text = region
