ca507fbf63fc7953fcbc7b986dfcdc59f6cc014c
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-20 16:29:54 +0900

    fix - 캠핑장 아이템 UI 수정

--- app/src/main/java/com/example/campwith/presentation/camplist/adapter/CampListAdapter.kt
+++ app/src/main/java/com/example/campwith/presentation/camplist/adapter/CampListAdapter.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.campwith.R
 import com.example.campwith.data.camp.CampItem
 import com.example.campwith.data.camp.CampResponse
