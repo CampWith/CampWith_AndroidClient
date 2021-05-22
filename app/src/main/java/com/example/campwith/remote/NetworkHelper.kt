2a212d3db5a58d3b28da0c68d42bcff28280216d
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-22 23:28:22 +0900

    fix - baseUrl 수정

--- app/src/main/java/com/example/campwith/remote/NetworkHelper.kt
+++ app/src/main/java/com/example/campwith/remote/NetworkHelper.kt
@@ -22,7 +22,7 @@ object NetworkHelper {
     private val gson = GsonBuilder().setLenient().create()
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://15.165.203.28:8080/")
+        .baseUrl("http://13.209.43.90:8080/")
         .client(okHttpClent)
         .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
         .addConverterFactory(GsonConverterFactory.create(gson))
