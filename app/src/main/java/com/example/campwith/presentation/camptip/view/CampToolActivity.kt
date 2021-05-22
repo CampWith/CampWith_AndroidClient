63261f2155b87880c8ca430c7af53c387cd64b49
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-23 00:03:27 +0900

    add - 캠핑 도구 액티비티 추가

---
+++ app/src/main/java/com/example/campwith/presentation/camptip/view/CampToolActivity.kt
@@ -0,0 +1,12 @@
+package com.example.campwith.presentation.camptip.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.campwith.R
+
+class CampToolActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camp_tool)
+    }
+}
