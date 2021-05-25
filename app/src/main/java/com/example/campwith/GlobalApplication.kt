625280ec774568631c3a3e56fee1fc31539ad264
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-25 19:57:05 +0900

    add - 카카오 로그인 구현

--- app/src/main/java/com/example/campwith/GlobalApplication.kt
+++ app/src/main/java/com/example/campwith/GlobalApplication.kt
@@ -6,16 +6,19 @@ import com.naver.maps.map.NaverMapSdk
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import com.kakao.sdk.common.KakaoSdk
 
-class MyApplication : Application() {
+class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         NaverMapSdk.getInstance(this).client =
             NaverMapSdk.NaverCloudPlatformClient("acdbyskku1")
 
+        KakaoSdk.init(this, getString(R.string.kakao_app_key))
+
         startKoin {
             androidLogger()
-            androidContext(this@MyApplication)
+            androidContext(this@GlobalApplication)
             modules(myDiModule)
         }
     }
