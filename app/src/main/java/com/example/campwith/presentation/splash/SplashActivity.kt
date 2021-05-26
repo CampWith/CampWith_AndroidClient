ca62e67cf5ac906d82287b6df2c0f0d3913d2dc8
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-26 17:17:46 +0900

    add - 스플래시 애니메이션 설정

--- app/src/main/java/com/example/campwith/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/splash/SplashActivity.kt
@@ -1,45 +1,55 @@
 package com.example.campwith.presentation.splash
 
+import android.animation.Animator
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.campwith.R
 import com.example.campwith.presentation.login.LoginActivity
 import com.example.campwith.presentation.main.view.MainActivity
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        val mainIntent = Intent(this, MainActivity::class.java)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-            UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
-                if (error != null) {
-                    Log.e(TAG, "토큰 정보 보기 실패", error)
-                    val intent = Intent(this, LoginActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else if (tokenInfo != null) {
-                    Log.i(
-                        TAG, "토큰 정보 보기 성공" +
-                                "\n회원번호: ${tokenInfo.id}" +
-                                "\n만료시간: ${tokenInfo.expiresIn} 초"
-                    )
-                    val intent = Intent(this, MainActivity::class.java)
-                    startActivity(intent)
-                    finish()
+        splash.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) {
+            }
+
+            override fun onAnimationEnd(p0: Animator?) {
+                UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+                    if (error != null) {
+                        Log.e(TAG, "토큰 정보 보기 실패", error)
+                        startActivity(loginIntent)
+                        finish()
+                    } else if (tokenInfo != null) {
+                        Log.i(
+                            TAG, "토큰 정보 보기 성공" +
+                                    "\n회원번호: ${tokenInfo.id}" +
+                                    "\n만료시간: ${tokenInfo.expiresIn} 초"
+                        )
+                        startActivity(mainIntent)
+                        finish()
+                    }
                 }
             }
 
-        }, SPLASH_DELAY_TIME)
+            override fun onAnimationCancel(p0: Animator?) {
+            }
+
+            override fun onAnimationRepeat(p0: Animator?) {
+            }
+        })
     }
 
     companion object {
         const val TAG = "SplashActivity"
-        private const val SPLASH_DELAY_TIME: Long = 1500
     }
 }
