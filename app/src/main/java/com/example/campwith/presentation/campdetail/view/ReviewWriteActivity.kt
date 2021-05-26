57e4d22c9343188e393f87408aa689432e8a84e3
Haemin-Park <phm9368@sookmyung.ac.kr>
2021-05-26 09:33:29 +0900

    add - 리뷰 작성 액티비티 구현

--- app/src/main/java/com/example/campwith/presentation/campdetail/view/ReviewWriteActivity.kt
+++ app/src/main/java/com/example/campwith/presentation/campdetail/view/ReviewWriteActivity.kt
@@ -1,19 +1,41 @@
 package com.example.campwith.presentation.campdetail.view
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.campwith.R
+import com.example.campwith.data.camp.response.ReviewResponseItem
 import com.example.campwith.databinding.ActivityReviewWriteBinding
 import com.example.campwith.presentation.base.BaseActivity
 import com.example.campwith.presentation.campdetail.viewmodel.CampReviewWriteViewModel
+import kotlinx.android.synthetic.main.activity_review_write.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ReviewWriteActivity :
     BaseActivity<ActivityReviewWriteBinding, CampReviewWriteViewModel>(R.layout.activity_review_write) {
     override val viewModel: CampReviewWriteViewModel by viewModel()
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val campId = intent.getStringExtra("campId")
+        val campNm = intent.getStringExtra("campNm")
+
+        binding.tvNm.text = campNm
+
+        container_review.setOnTouchListener { view, motionEvent ->
+            if (binding.editTextReview.isFocused) {
+                hideKeyboard()
+                false
+            } else {
+                false
+            }
+        }
 
         this.runOnUiThread {
             binding.toolbarActivityCampReviewWrite.run {
@@ -23,5 +45,30 @@ class ReviewWriteActivity :
                 setCancleBtnClick(View.OnClickListener { finish() })
             }
         }
+
+        binding.tvReviewRegister.setOnClickListener {
+            val result = Intent().apply {
+                putExtra(
+                    "Review",
+                    ReviewResponseItem(
+                        binding.editTextReview.text.toString(),
+                        binding.ratingBarReviewWrite.rating,
+                        "2099.99.99",
+                        "테스트"
+                    )
+                )
+            }
+            setResult(Activity.RESULT_OK, result)
+            finish()
+        }
+    }
+
+    private fun hideKeyboard() {
+        val inputManager: InputMethodManager =
+            this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputManager.hideSoftInputFromWindow(
+            this.currentFocus!!.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
     }
 }
